ab1383f4687c441d32ad487f7f379d02d2f02d7d
DESKTOP-UDOVMPI\henry <grtert@gmail.com>
2021-06-11 02:00:53 +0800

    ASPNetCoreMVC

---
+++ AspNetCoreMVC5/AspNetCoreMVC5/Controllers/HelloworldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC5.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        // index & welcome
+        // get action
+
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        // https://localhost:44336/Helloworld/WelcomeMan?name=test&numTimes=99
+        // 用 HtmlEncoder.Default.Encode 來保護應用程式免于惡意輸入，例如透過 JavaScript。
+        public string WelcomeMan(string name, int numTimes = 1)
+        {
+            // return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        public string WelcomeID(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
